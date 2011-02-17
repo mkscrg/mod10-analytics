1c08c81533e5db8a3251060c84bb51c8c40c809c
Michael S. Craig <mkscrg@gmail.com>
2011-02-17 02:57:51 -0500

    Scrape needs to append stats to a file

--- README.markdown
+++ README.markdown
@@ -1,6 +1,6 @@
 ## mod10-analytics
 
 This is a small Haskell project to explore various aspects of the Mod10 card
-game. It is a work in progress, and currently undocumented. (The latter fact
-will change soon, although I think the code is clear and readable as it
+game. It is a work in progress, and currently mostly undocumented. (The latter
+fact will change soon, although I think the code is clear and readable as it
 stands.)
--- Scrape.hs
+++ Scrape.hs
@@ -2,82 +2,117 @@ module Main where
 
 
 import Data.Foldable ( foldl' )
-import Data.List ( intersperse
-                 , isPrefixOf )
+import Data.List ( intersperse )
 import System ( getArgs )
-import System.IO ( hClose
-                 , hGetContents
-                 , openFile
-                 , IOMode(..) )
 
 
 main :: IO ()
 main = do
     argv <- getArgs
-    inH <- openFile (head argv) ReadMode
-    raw <- hGetContents inH
-    let rs = getRawStats raw
-    let cs = getCleanStats rs
-    putStrLn $ show cs ++ "\n"
-    hClose inH
-
-
-getRawStats :: String -> RawStats
-getRawStats raw = collect (filter isEndGame (lines raw)) $
-    RawStats { winCounts = []
-             , lossCounts = []
-             , timeouts = 0 }
-  where
-    collect []       sts = sts
-    collect (rl:rls) sts =
-        case read rl of
-          Win i   -> collect rls
-                          sts { winCounts = i : winCounts sts }
-          Loss i  -> collect rls
-                          sts { lossCounts = i : lossCounts sts }
-          Timeout -> collect rls
-                          sts { timeouts = 1 + timeouts sts }
-    isEndGame ln | "Win"     `isPrefixOf` ln = True
-    isEndGame ln | "Loss"    `isPrefixOf` ln = True
-    isEndGame ln | "Timeout" `isPrefixOf` ln = True
-    isEndGame _                              = False
+    raw <- readFile $ head argv
+    let stats = getRawStats raw
+    print $ getCleanStats stats
 
 
 getCleanStats :: RawStats -> CleanStats
-getCleanStats RawStats {winCounts=winC, lossCounts=lossC, timeouts=timeN} = 
+getCleanStats stats =
     CleanStats { count = n
-               , winFreq = freq winN n
-               , winMeanCount = winMeC
-               , winStdDevCount = stdDev winC winN winMeC
-               , lossFreq = freq lossN n
-               , lossMeanCount = lossMeC
-               , lossStdDevCount = stdDev lossC lossN lossMeC
-               , timeoutFreq = freq timeN n }
+               , winNFrac = nFrac (winN stats) n
+               , winMinMax = (winMin stats, winMax stats)
+               , winMeanSD = meanSD (winN stats) (winSumX stats)
+                                    (winSumXX stats)
+               , lossNFrac = nFrac (lossN stats) n
+               , lossMinMax = (lossMin stats, lossMax stats)
+               , lossMeanSD = meanSD (lossN stats) (lossSumX stats)
+                                    (lossSumXX stats)
+               , timeoutNFrac = nFrac (timeoutN stats) n }
   where
-    winMeC = mean winC winN
-    lossMeC = mean lossC lossN
-    winN = length winC
-    lossN = length lossC
-    n = winN + lossN + timeN
+    n = winN stats + lossN stats + timeoutN stats
 
 
-stdDev :: [Int] -> Int -> Double -> Double
-stdDev xis n xbar = (**) (1 / (fromIntegral n - 1) *
-                          (sum' (map sqdiff xis))) 0.5
+getRawStats :: String -> RawStats
+getRawStats raw = foldl' collect blankRawStats $ filter isEndGame $ lines raw
   where
-    sqdiff c = (fromIntegral c - xbar) ** 2
+    collect stats str =
+        case read str of
+          Win i   ->  stats { winN = winN stats + 1
+                            , winMin = min i $ winMin stats
+                            , winMax = max i $ winMax stats
+                            , winSumX = winSumX stats + i
+                            , winSumXX = winSumXX stats + sqr i }
+          Loss i  ->  stats { lossN = lossN stats + 1
+                            , lossMin = min i $ lossMin stats
+                            , lossMax = max i $ lossMax stats
+                            , lossSumX = lossSumX stats + i
+                            , lossSumXX = lossSumXX stats + sqr i }
+          Timeout ->  stats { timeoutN = timeoutN stats + 1 }
+    blankRawStats = RawStats { winN = 0
+                             , winMin = 1500
+                             , winMax = 0
+                             , winSumX = 0
+                             , winSumXX = 0
+                             , lossN = 0
+                             , lossMin = 1500
+                             , lossMax = 0
+                             , lossSumX = 0
+                             , lossSumXX = 0
+                             , timeoutN = 0 }
+    isEndGame str = or $ map (== head str) "WLT"
+    sqr i = fromIntegral $ i^(2::Int)
 
 
-freq :: Int -> Int -> Double
-freq a b = fromIntegral a / fromIntegral b
+nFrac :: Int -> Int -> (Int, Double)
+nFrac m n = (m, m' / fromIntegral n)
+  where
+    m' = fromIntegral m
+
+meanSD :: Int -> Int -> Integer -> (Double, Double)
+meanSD n sumX sumXX =
+    (xBar, ((n' * xBar**2 - 2 * xBar * sumX' + sumXX') / (n' - 1))**0.5)
+  where
+    xBar = sumX' / n'
+    sumXX' = fromIntegral sumXX
+    sumX' = fromIntegral sumX
+    n' = fromIntegral n
 
 
-mean :: [Int] -> Int -> Double
-mean xis n = fromIntegral (sum' xis) / fromIntegral n
+data CleanStats = CleanStats { count :: Int
+                             , winNFrac :: (Int, Double)
+                             , winMinMax :: (Int, Int)
+                             , winMeanSD :: (Double, Double)
+                             , lossNFrac :: (Int, Double)
+                             , lossMinMax :: (Int, Int)
+                             , lossMeanSD :: (Double, Double)
+                             , timeoutNFrac :: (Int, Double) }
+
+instance Show CleanStats where
+    show sts = (concat $ intersperse "\n"
+        [ "# games played: " ++ show (count sts)
+        , "winning:"
+        , "      (n,frac): " ++ show (winNFrac sts)
+        , "     (min,max): " ++ show (winMinMax sts)
+        , "  (mean,stDev): " ++ show (winMeanSD sts)
+        , "losing:"
+        , "      (n,frac): " ++ show (lossNFrac sts)
+        , "     (min,max): " ++ show (lossMinMax sts)
+        , "  (mean,stDev): " ++ show (lossMeanSD sts)
+        , "timeout:"
+        , "      (n,frac): " ++ show (timeoutNFrac sts) ])
+        ++ "\n"
 
 
-sum' :: (Num a) => [a] -> a
-sum' = foldl' (+) 0
+data RawStats = RawStats { winN :: !Int
+                         , winMin :: !Int
+                         , winMax :: !Int
+                         , winSumX :: !Int
+                         , winSumXX :: !Integer
+                         , lossN :: !Int
+                         , lossMin :: !Int
+                         , lossMax :: !Int
+                         , lossSumX :: !Int
+                         , lossSumXX :: !Integer
+                         , timeoutN :: !Int
+                         } deriving (Show)
 
 
 data GameEnds = Win Int
@@ -85,30 +120,3 @@ data GameEnds = Win Int
               | Timeout
                 deriving ( Read
                          , Show )
-
-
-data CleanStats = CleanStats { count :: Int
-                             , winFreq :: Double
-                             , winMeanCount :: Double
-                             , winStdDevCount :: Double
-                             , lossFreq :: Double
-                             , lossMeanCount :: Double
-                             , lossStdDevCount :: Double
-                             , timeoutFreq :: Double }
-
-instance Show CleanStats where
-    show sts = concat $ intersperse "\n" $
-               [ "games played: " ++ show (count sts)
-               , "win frequency: " ++ show (winFreq sts * 100) ++ "%"
-               , "winning mean count: " ++ show (winMeanCount sts)
-               , "winning std. dev. count: " ++ show (winStdDevCount sts)
-               , "loss frequency: " ++ show (lossFreq sts * 100) ++ "%"
-               , "losing mean count: " ++ show (lossMeanCount sts)
-               , "losing std. dev. count: " ++ show (lossStdDevCount sts)
-               , "timeout frequency: " ++ show (timeoutFreq sts * 100) ++ "%" ]
-
-
-data RawStats = RawStats { winCounts :: [Int]
-                         , lossCounts :: [Int]
-                         , timeouts :: Int
-                         } deriving (Show)
