8f79bd6203b7831837ceead150cd2e2a2d90d7d5
Michael S. Craig <mkscrg@gmail.com>
2011-02-18 13:24:13 -0500

    separate plotting from scraping

--- Scrape.hs
+++ Scrape.hs
@@ -1,49 +1,23 @@
-module Main where
+module Main ( main ) where
 
 
 import Control.DeepSeq ( deepseq )
-import Data.Array ( listArray )
 import Data.Foldable ( foldl' )
 import Data.List ( intersperse )
 import System ( getArgs )
-import System.Exit ( ExitCode )
-
-import qualified Graphics.Gnuplot.Advanced as Plot
-import qualified Graphics.Gnuplot.MultiPlot as MultiPlot
-import qualified Graphics.Gnuplot.Terminal.PNG as PNG
-import qualified Graphics.Gnuplot.Plot.TwoDimensional as Plot2D
-import qualified Graphics.Gnuplot.Graph.TwoDimensional as Graph2D
-import qualified Graphics.Gnuplot.Frame.Option as Opt
-import qualified Graphics.Gnuplot.Frame.OptionSet as Opts
-import qualified Graphics.Gnuplot.Frame as Frame
 
 
-main :: IO ExitCode
+main :: IO ()
 main = do
     argv <- getArgs
-    raw <- readFile $ head argv
+    let fname = head argv
+    raw <- readFile fname
     let stats = getRawStats raw
-    print $ getCleanStats stats
-    makeplot stats $ head argv
-
-
-makeplot :: RawStats -> FilePath -> IO ExitCode
-makeplot stats statfile =
-    Plot.plot term $ MultiPlot.simpleFromFrameArray $
-    listArray ((0::Int, 0::Int), (1, 0)) frames
-  where
-    frames = zipWith Frame.cons options $
-             map (Plot2D.list Graph2D.listLines)
-             [lossHist stats, winHist stats]
-    options = map (\o -> o genOpt) [topOpt, botOpt]
-    topOpt = Opts.add (Opt.custom "bmargin") ["0.0"] .
-             Opts.remove Opt.xTicks
-    botOpt = Opts.add (Opt.custom "tmargin") ["0.0"] .
-             Opts.add Opt.xTicks ["nomirror"]
-    genOpt = Opts.remove Opt.yTicks $
-             Opts.remove Opt.key Opts.deflt
-    term = PNG.fontSmall $
-           PNG.cons $ fst (break (== '.') statfile) ++ ".png"
+    putStrLn fname
+    putStrLn $ show $ getCleanStats stats
+    putStrLn $ " win histogram: " ++ (show (winHist stats))
+    putStrLn $ "loss histogram: " ++ (show (lossHist stats))
+    putStrLn ""
 
 
 getCleanStats :: RawStats -> CleanStats
@@ -129,19 +103,18 @@ data CleanStats = CleanStats { count :: Int
                              , timeoutNFrac :: (Int, Double) }
 
 instance Show CleanStats where
-    show sts = (concat $ intersperse "\n"
-        [ "# games played: " ++ show (count sts)
-        , "winning:"
+    show sts = concat $ intersperse "\n"
+        [ "  games played: " ++ show (count sts)
+        , "winning "
         , "      (n,frac): " ++ show (winNFrac sts)
         , "     (min,max): " ++ show (winMinMax sts)
         , "  (mean,stDev): " ++ show (winMeanSD sts)
-        , "losing:"
+        , "losing "
         , "      (n,frac): " ++ show (lossNFrac sts)
         , "     (min,max): " ++ show (lossMinMax sts)
         , "  (mean,stDev): " ++ show (lossMeanSD sts)
-        , "timeout:"
-        , "      (n,frac): " ++ show (timeoutNFrac sts) ])
-        ++ "\n"
+        , "timeout "
+        , "      (n,frac): " ++ show (timeoutNFrac sts) ]
 
 
 data RawStats = RawStats { winN :: !Int
